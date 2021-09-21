98c1206c08ac5de39f7309bf06a39ed6c8298ff3
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-21 21:18:03 +0530

    convert date into string

--- app.js
+++ app.js
@@ -9,4 +9,23 @@ function reverseString(str) {
     var reversedString = reverseString(str);
     return str === reversedString;
   }
+
+  function convertDateToString(date){
+    var dateStr = { day: "", month: "", year: "" };
+  
+    if (date.day < 10) {
+    dateStr.day = "0" + date.day;
+} else {
+  dateStr.day = date.day.toString();
+}
+
+if (date.month < 10) {
+  dateStr.month = "0" + date.month;
+} else {
+  dateStr.month = date.month.toString();
+}
+
+dateStr.year = date.year.toString();
+return dateStr;
+}
   
