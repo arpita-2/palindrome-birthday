c77fc69d1520ddd76464a989fae34479d7e3bfdf
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-21 21:38:22 +0530

    next palindrome date

--- app.js
+++ app.js
@@ -51,4 +51,13 @@ function getDateInAllFormats(date) {
     return palindromeList;
   }
   
+  function isLeapYear(year) {
+    if (year % 400 === 0) return true;
+  
+    if (year % 100 === 0) return false;
+  
+    if (year % 4 === 0) return true;
+  
+    return false;
+  }
   
